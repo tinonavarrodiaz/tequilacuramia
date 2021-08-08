5dcf972bdaf2ae5408b19c23b857679d43052677
Florentino Navarro <tino.navarro@hotmail.com>
2021-08-08 14:02:40 -0500

    done

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -32,28 +32,37 @@ class Slider extends Component {
       }
     }, 5000);
   }
-  slideUp(){
-  }
-  slideDown(){
+
+  bullActive = (e) =>{
+    let Target = e.target
+    let index = parseInt(Target.dataset.index)
+    // console.log([...Target.parentElement.parentElement.querySelectorAll('.Slider__item')])
+    let cocktails = [...Target.parentElement.parentElement.querySelectorAll('.Slider__item')]
+    let bullets = [...Target.parentElement.querySelectorAll('li')]
+    // console.log(bullets)
+    cocktails.map(el=>el.classList.remove('active'))
+    cocktails[index].classList.add('active')
+    bullets.map(el=>el.classList.remove('active'))
+    Target.classList.add('active')
   }
   render(){ 
     return(
       <div className="Slider-container">
         <div className="Slider" id="sliderNH" >
-          <img src={imgs.slider1} data-index="0" onClick={this.next} className="Slider__item active" alt="banner" />
-          <img src={imgs.slider2} data-index="1" onClick={this.next} className="Slider__item" alt="banner" />
-          <img src={imgs.slider3} data-index="2" onClick={this.next} className="Slider__item" alt="banner" />
-          <img src={imgs.slider4} data-index="3" onClick={this.next} className="Slider__item" alt="banner" />
-          <img src={imgs.slider5} data-index="4" onClick={this.next} className="Slider__item" alt="banner" />
-          <img src={imgs.slider6} data-index="5" onClick={this.next} className="Slider__item" alt="banner" />
+          <img src={imgs.slider1} data-index="0" className="Slider__item active" alt="banner" />
+          <img src={imgs.slider2} data-index="1" className="Slider__item" alt="banner" />
+          <img src={imgs.slider3} data-index="2" className="Slider__item" alt="banner" />
+          <img src={imgs.slider4} data-index="3" className="Slider__item" alt="banner" />
+          <img src={imgs.slider5} data-index="4" className="Slider__item" alt="banner" />
+          <img src={imgs.slider6} data-index="5" className="Slider__item" alt="banner" />
         </div>
         <ul className="bullets">
-          <li className="bull active"></li>
-          <li className="bull"></li>
-          <li className="bull"></li>
-          <li className="bull"></li>
-          <li className="bull"></li>
-          <li className="bull"></li>
+          <li style={{cursor: "pointer"}}onClick={this.bullActive} data-index="0" className="bull active"></li>
+          <li style={{cursor: "pointer"}}onClick={this.bullActive} data-index="1" className="bull"></li>
+          <li style={{cursor: "pointer"}}onClick={this.bullActive} data-index="2" className="bull"></li>
+          <li style={{cursor: "pointer"}}onClick={this.bullActive} data-index="3" className="bull"></li>
+          <li style={{cursor: "pointer"}}onClick={this.bullActive} data-index="4" className="bull"></li>
+          <li style={{cursor: "pointer"}}onClick={this.bullActive} data-index="5" className="bull"></li>
         </ul>
       </div>
     )
