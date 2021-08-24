8a885471754976122276032a4615615fba207ba1
Florentino Navarro <tino.navarro@hotmail.com>
2021-08-24 18:49:45 -0500

    230820211849

--- src/components/ArticleText.js
+++ src/components/ArticleText.js
@@ -8,11 +8,9 @@ class ArticleText extends Component {
         
           <img src={imagesHome.svg} alt="Hecho en Mexico" data-aos="zoom-in"/>
         {/* </div> */}
-          <p style={{marginTop: '50px'}} data-aos="fade-right">Many generations ago, the most beautiful lady in Mexico was born. The town
-            couldn’t stop talking about her beauty.<br />It is said that her physical fairness paled in comparison to
+          <p style={{marginTop: '50px'}} data-aos="fade-right">Many generations ago, the most beautiful lady in Mexico was born. The town could not stop talking about her beauty.<br />It is said that her physical fairness paled in comparison to
             her charitable and unselfish heart.<br />Our heroine changed the lives of many.</p>
-            <p className="slogan__text" data-aos="fade-left" >As the years went by, our beauty grew older andeventually time blossomed truth into mystery leaving
-                some to wonder, what legacy did she leave behind</p>
+            <p className="slogan__text" data-aos="fade-left" >As the years went by, our beauty grew older and eventually time blossomed truth into mystery Leaving some to wonder what her legacy left behind</p>
       </article>
     )
   }
